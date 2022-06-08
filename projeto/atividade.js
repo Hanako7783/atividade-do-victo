ae654cf8185f64736ae79e7252e847493aac7782
Hanako7783 <hbmhad@gmail.com>
2022-06-08 17:52:13 -0500

    soma e etc

--- projeto/atividade.js
+++ projeto/atividade.js
@@ -27,8 +27,39 @@ n6:letraa,
 
 
 }
+
 const json = JSON.stringify(lista3)
 
 const json2 = JSON.parse(json)
 
 console.log(json2)
+
+
+const [n1,n2,n3,n4,n5,n6,] = lista2
+console.log(n1)
+console.log(n2)
+console.log(n3)
+
+console.log(n4)
+console.log(n5)
+console.log(n6)
+
+const lista5 ={
+    n1:n1,
+    n2:n2,
+    n3:n3,
+    n4:n4,
+    n5:n5,
+    n6:n6,
+}
+
+const json7 = JSON.stringify(lista5)
+
+const json8 = JSON.parse(json7)
+
+console.log(json8)
+
+console.log(n1+n2)
+console.log(n1-n2)
+console.log(n1*n2)
+console.log(n1/n2)
